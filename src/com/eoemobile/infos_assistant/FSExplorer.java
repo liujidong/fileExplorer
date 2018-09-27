a857bcbfb28d5377c271bfdfcedcdbb031f27f1c
liujidong <wwwliujidong@163.com>
2018-09-27 17:46:01 +0800

    基础版本示例

---
+++ src/com/eoemobile/infos_assistant/FSExplorer.java
@@ -0,0 +1,121 @@
+package com.eoemobile.infos_assistant;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+public class FSExplorer extends Activity implements OnItemClickListener {
+	private static final String TAG = "FSExplorer";
+	//private static final int IM_PARENT = Menu.FIRST + 1;
+	//private static final int IM_BACK = IM_PARENT + 1;
+ 
+	ListView itemlist = null;
+	String path = Environment.getDataDirectory().getPath();
+	List<Map<String, Object>> list;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.files);
+		setTitle("文件浏览器");
+		Bundle extras = getIntent().getExtras();
+		Log.i(TAG, "path="+path);
+		if (extras != null) {
+			int position = extras.getInt("position");
+			if(position==1){
+				path = Environment.getExternalStorageDirectory().getPath();//"/sdcard";
+				Log.i(TAG, "path="+path);
+			}
+		}
+		itemlist = (ListView) findViewById(R.id.itemlist);
+		refreshListItems(path);
+	}
+
+	private void refreshListItems(String path) {
+		setTitle("文件浏览器 > "+path);
+		list = buildListForSimpleAdapter(path);
+		SimpleAdapter notes = new SimpleAdapter(this, list, R.layout.file_row,
+				new String[] { "name", "path" ,"img"}, new int[] { R.id.name,
+						R.id.desc ,R.id.img});
+		itemlist.setAdapter(notes);
+		itemlist.setOnItemClickListener(this);
+		itemlist.setSelection(0);
+	}
+
+	private List<Map<String, Object>> buildListForSimpleAdapter(String path) {
+		File[] files = new File(path).listFiles();
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>(files.length);
+		Map<String, Object> root = new HashMap<String, Object>();
+		root.put("name", "主目录");
+		root.put("img", R.drawable.file_root);
+		root.put("path", "go to index directory");
+		list.add(root);
+		Map<String, Object> pmap = new HashMap<String, Object>();
+		pmap.put("name", "..");
+		pmap.put("img", R.drawable.file_paranet);
+		pmap.put("path", "go to paranet Directory");
+		list.add(pmap);
+		for (File file : files){
+			Map<String, Object> map = new HashMap<String, Object>();
+			if(file.isDirectory()){
+				map.put("img", R.drawable.directory);
+			}else{
+				map.put("img", R.drawable.file_doc);
+			}
+			map.put("name", file.getName());
+			map.put("path", file.getPath());
+			list.add(map);
+		}
+		return list;
+	}
+	
+	private void goToParent() {
+		File file = new File(path);
+		File str_pa = file.getParentFile();
+		if(str_pa == null){
+			Toast.makeText(FSExplorer.this,
+					getString(R.string.is_root_dir),
+					Toast.LENGTH_SHORT).show();
+			refreshListItems(path);	
+		}else{
+			path = str_pa.getAbsolutePath();
+			refreshListItems(path);	
+		}
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+		//Log.i(TAG, "item clicked! [" + position + "]");
+		if (position == 0) {
+			finish();
+		}else if(position == 1){
+			goToParent();
+		} else {
+			path = (String) list.get(position).get("path");
+			File file = new File(path);
+			if (file.isDirectory())
+				refreshListItems(path);
+			else
+				Toast.makeText(FSExplorer.this,
+						getString(R.string.is_file),
+						Toast.LENGTH_SHORT).show();
+		}
+
+	}
+
+ 
+
+}
---
+++ src/com/eoemobile/infos_assistant/MainActivity.java
@@ -0,0 +1,72 @@
+package com.eoemobile.infos_assistant;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class MainActivity extends Activity implements OnItemClickListener {
+	private static final String TAG = "eoeInfosAssistant";
+	ListView itemlist = null;
+	List<Map<String, Object>> list;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		itemlist = (ListView) findViewById(R.id.itemlist);
+		refreshListItems();
+	}
+
+	private void refreshListItems() {
+		list = buildListForSimpleAdapter();
+		SimpleAdapter notes = new SimpleAdapter(this, list, R.layout.item_row,
+				new String[] { "name", "desc", "img" }, new int[] { R.id.name,
+						R.id.desc, R.id.img });
+		itemlist.setAdapter(notes);
+		itemlist.setOnItemClickListener(this);
+		itemlist.setSelection(0);
+	}
+
+	private List<Map<String, Object>> buildListForSimpleAdapter() {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>(3);
+		// Build a map for the attributes
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map = new HashMap<String, Object>();
+		map.put("name", "内部存储");
+		map.put("desc", "浏览查看文件系统.");
+		map.put("img", R.drawable.file_explorer);
+		list.add(map);
+		if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
+			map = new HashMap<String, Object>();
+			map.put("name", "外部sdcard存储");
+			map.put("desc", "浏览查看文件系统.");
+			map.put("img", R.drawable.file_explorer);
+			list.add(map);
+		}
+		
+		return list;
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+		Intent intent = new Intent();
+		Log.i(TAG, "item clicked! [" + position + "]");
+		intent.setClass(MainActivity.this, FSExplorer.class);
+		intent.putExtra("position", position);
+		startActivity(intent);
+	}
+}
